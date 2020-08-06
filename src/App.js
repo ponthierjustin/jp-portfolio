efa169828ac8bf4d9000e6622a79a22a0adcf7ff
Justin <ponthierjustin@gmail.com>
2020-08-05 20:31:05 -0400

    fixed design and added spacing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import "./App.css";
 
 
 function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 //Header
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -20,13 +20,14 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
-    textAlign: "center",
+    fontFamily: "Sora"
   },
+  button: {
+    padding: theme.spacing(2),
+    fontFamily: "Sora"
+  }
 }));
 
 export default function CenteredGrid() {
@@ -34,42 +35,49 @@ export default function CenteredGrid() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
+      <Grid container spacing={2}>
         <Grid item xs={12}>
-        <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h1" className={classes.title}>
             Justin Ponthier
           </Typography>
-
-          <IconButton edge="start"  color="inherit"  target="_blank" href="https://github.com/ponthierjustin" >
-            <GitHubIcon fontSize="large" />
+        <Button className={classes.button}>
+        About
+        </Button>
+          <Button className={classes.button}>
+        Resume
+        </Button>
+        <Button className={classes.button}>
+        Contact 
+       </Button>
+       
+       <IconButton edge="start"  color="inherit"  target="_blank" href="https://github.com/ponthierjustin" >
+            <GitHubIcon className={classes.button} fontSize="large" />
           </IconButton>
           
           <IconButton edge="start" color="inherit" target="_blank" href="https://www.linkedin.com/in/justin-ponthier-1509a8174/">
-            <LinkedInIcon fontSize="large"/>
+            <LinkedInIcon className={classes.button} fontSize="large"/>
           </IconButton>
         </Toolbar>
-      </AppBar>
         </Grid>
        
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <Paper className={classes.paper}>Bio</Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <Paper className={classes.paper}>Projects</Paper>
+        </Grid>
+        <Grid item xs={3} >
+        <Paper className={classes.paper}>Bio</Paper>
+        </Grid>
+        <Grid item xs={3}  >
+        <Paper className={classes.paper}>Bio</Paper>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+          <Paper className={classes.paper}>Graph</Paper>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+          <Paper className={classes.paper}>Graph</Paper>
         </Grid>
       </Grid>
     </div>
