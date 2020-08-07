ddb419a8586a62c7e101f133dc243c0ed6256dca
Justin <ponthierjustin@gmail.com>
2020-08-06 22:57:46 -0400

    Updates

---
+++ precache-manifest.06b1d07ec157ea74e79483002424e1c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d191e2aedf57f080a2c1d9b9dd8a3dbd",
+    "url": "/jp-portfolio/index.html"
+  },
+  {
+    "revision": "ca3a384e0e82c31dac59",
+    "url": "/jp-portfolio/static/css/main.f6b8f2df.chunk.css"
+  },
+  {
+    "revision": "158f931f2dc60f7a0792",
+    "url": "/jp-portfolio/static/js/2.6f1d7ff1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jp-portfolio/static/js/2.6f1d7ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3a384e0e82c31dac59",
+    "url": "/jp-portfolio/static/js/main.57b94b3f.chunk.js"
+  },
+  {
+    "revision": "bc8237386c9d09200d80",
+    "url": "/jp-portfolio/static/js/runtime-main.7ac84f9a.js"
+  }
+]);
